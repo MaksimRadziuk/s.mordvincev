ce8e4e6d311c934d9a62ebb0b2df058fe1b4d9cf
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2017-09-28 04:17:28 +0300

    final commit 28/09 4.17

--- template/js/stub.js
+++ template/js/stub.js
@@ -1,18 +1,19 @@
-//анимация печатания текста
-/*import Typed from 'typed.min.js';
-
-var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 40
-};
-
-var typed = new Typed(".element", options);*/
-$(function() {
-    $(#typed).typed ({
-        strings:['- приносит <p>миллионы</p>'],
-        typeSpeed: 30,
-        backDelay: 500,
-        loop: false,
-        loopCount:false,
-    });
+$(document).ready(function() {
+    $('body #section').eq(0).addClass("active").fadeIn(1000); // Показываем первый блок, можно и не первый, если прописать нужную цифру в eq()
+    setInterval('blockAnimate();', 8000); // Вызываем функцию для смены блока каждые 5 секунд
 });
+
+// Функция для смены блоков, показывает блоки по очереди, начальный блок задаётся выше
+function blockAnimate() {
+    var length = $('body #section').length - 1;
+    $('body #section').each(function(index) {
+        if($(this).hasClass('active') && index != length) {
+            $(this).removeClass("active").fadeOut(1000).next('#section').addClass("active").delay(1000).fadeIn(1000);
+            return false;
+        } else if (index == length) {
+            $(this).removeClass('active').fadeOut(1000);
+            $('body #section').eq(0).addClass("active").delay(1000).fadeIn(1000);
+            return false;
+        }
+    });
+};
