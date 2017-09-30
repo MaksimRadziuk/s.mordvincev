8cba9d52f1a80990b55055055bb88567759f6223
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2017-10-01 00:08:07 +0300

    final commit 01/10 00.07

--- template/js/stub.js
+++ template/js/stub.js
@@ -1,19 +1,30 @@
+// ===== PRELOADER
+
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader');
+
+    $preloader.fadeOut();
+});
+
+
+
 $(document).ready(function() {
-    $('body #section').eq(0).addClass("active").fadeIn(1000); // Показываем первый блок, можно и не первый, если прописать нужную цифру в eq()
+    $('body .section').eq(0).addClass("active").fadeIn(1000); // Показываем первый блок, можно и не первый, если прописать нужную цифру в eq()
     setInterval('blockAnimate();', 8000); // Вызываем функцию для смены блока каждые 5 секунд
 });
 
 // Функция для смены блоков, показывает блоки по очереди, начальный блок задаётся выше
 function blockAnimate() {
-    var length = $('body #section').length - 1;
-    $('body #section').each(function(index) {
+    var length = $('body .section').length - 1;
+    $('body .section').each(function(index) {
         if($(this).hasClass('active') && index != length) {
-            $(this).removeClass("active").fadeOut(1000).next('#section').addClass("active").delay(1000).fadeIn(1000);
+            $(this).removeClass("active").fadeOut(1000).next('.section').addClass("active").delay(1000).fadeIn(1000);
             return false;
         } else if (index == length) {
             $(this).removeClass('active').fadeOut(1000);
-            $('body #section').eq(0).addClass("active").delay(1000).fadeIn(1000);
+            $('body .section').eq(0).addClass("active").delay(1000).fadeIn(1000);
             return false;
         }
     });
 };
+
